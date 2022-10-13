ec3719c98422fe38eca3bbd6e852f77d920aa177
Sanders <sndrs90@gmail.com>
2022-10-13 14:18:34 +0300

    Explicit intent to launch app from list

--- app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -41,19 +41,38 @@ class NerdLauncherActivity : AppCompatActivity() {
         })
 
         Log.i(TAG, "Found ${activities.size} activities")
-        
+
         recyclerView.adapter = ActivityAdapter(activities)
     }
 
     private class ActivityHolder(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
+        RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+
         private val nameTextView = itemView as TextView
         private lateinit var resolveInfo: ResolveInfo
+
+        init {
+            nameTextView.setOnClickListener(this)
+        }
+
         fun bindActivity(resolveInfo: ResolveInfo) {
             this.resolveInfo = resolveInfo
             val packageManager = itemView.context.packageManager
             val appName = resolveInfo.loadLabel(packageManager).toString()
             nameTextView.text = appName
+        }
+
+        override fun onClick(view: View) {
+            val activityInfo = resolveInfo.activityInfo
+            
+            val intent = Intent(Intent.ACTION_MAIN).apply {
+                setClassName(activityInfo.applicationInfo.packageName,
+                    activityInfo.name)
+            }
+
+            val context = view.context
+            context.startActivity(intent)
         }
     }
 
